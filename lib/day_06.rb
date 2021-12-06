de1557548568893dbdc2f168d2bc907347c3ea04
Sophie Gill <sophie-gill@hotmail.com>
2021-12-06 08:30:15 +0000

    day 6 part 2

--- lib/day_06.rb
+++ lib/day_06.rb
@@ -1,12 +1,12 @@
 input = File
-  .open('inputs/06/day_06.txt')
+  .open('inputs/06/test.txt')
   .readlines
   .map(&:chomp)
   .first
   .split(',')
   .map!(&:to_i)
 
-# Part 1
+p 'Part 1'
 
 def spawn_fish(fish, days)
   return fish if days == 0
@@ -26,5 +26,32 @@ def spawn_fish(fish, days)
   spawn_fish(result + new_fish, days-1)
 end
 
-p 'Part 1'
 p spawn_fish(input, 80).count
+
+p 'Part 2'
+
+def count_fish(fish)
+  counts = Array.new(9, 0)
+  fish.each { |day| counts[day] += 1 }
+
+  counts
+end
+
+def spawn_fish(fish_counts, days)
+  return fish_counts.reduce(:+) if days == 0
+
+  new_fish_counts = Array.new(9, 0)
+
+  fish_counts.each_with_index do |count, i|
+    if i == 0
+      new_fish_counts[8] += count
+      new_fish_counts[6] += count
+    else
+      new_fish_counts[i-1] += count
+    end
+  end
+
+  spawn_fish(new_fish_counts, days-1)
+end
+
+p spawn_fish(count_fish(input), 256)
